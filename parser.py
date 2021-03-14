aac6f020aacc32229a78c314174a637a6f4641d6
Dmitry Morozov <inood@yandex.ru>
2021-03-15 01:53:14 +0300

    add comment process

--- parser.py
+++ parser.py
@@ -7,31 +7,35 @@ from models import Pizza
 def main():
     success_count = 0
     HTML = open(FILE_NAME, 'r').read()
-    pattern = RE_PATTERN
-    matches = re.search(pattern, HTML)
-    json_data = matches.group(1)
-    data = json.loads(json_data)
+    matches = re.search(RE_PATTERN, HTML).group(1)
+    data = json.loads(matches)
     object_count = len(data)
     print(f'Найдено: {object_count} наименований пицц')
+
     for item in data:
         if item['category_id'] == 1:
             name = item['name']
             variants = item['variations']
             variants_count = len(variants)
             print(f'Обрабатывается {name} и {variants_count} вариантов ее исполнения\n')
+
             for variant in variants:
                 list_ingredients = []
                 ingredients = variant['include_ingredients']
+
                 for ingredient in ingredients:
                     list_ingredients.append(ingredient['name'])
+
                 price = variant['price']
                 size = variant['size']['value']
                 testo_type = variant['dough']
+
                 try:
                     Pizza(name, price, size, list_ingredients, testo_type).add()
                     success_count += 1
                 except:
                     print('ошибка обработки объекта')
+
     print(f'Обработка завершена\nОбработано: {success_count} записей')
 
 
